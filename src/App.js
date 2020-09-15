0e48a77483bfa0b4ef7c2ad17605a42838d0f0e8
gixxerblade <sdclarkie@gmail.com>
2020-09-15 11:54:36 -0400

    Add readme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const initialCityState = { city: "", state: "" };
+  const [zipcode, setZipcode] = useState("");
+  const [cityState, setCityState] = useState(initialCityState);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>City/State Lookup Tool</h1>
+      <form action="" className="form-data">
+        <label htmlFor="zip">Zip Code Here</label>
+        <input
+          value={zipcode}
+          placeholder="XXXXX"
+          type="text"
+          name="zip"
+          id="zip"
+          onChange={(event) => {
+            const { value } = event.target;
+            setZipcode(value.replace(/[^\d{5}]$/, "").substr(0, 5));
+          }}
+        />
+        <label htmlFor="city">City Here</label>
+        <input
+          value={cityState.city}
+          type="text"
+          name="city"
+          disabled
+          id="city"
+        />
+        <label htmlFor="state">State Here</label>
+        <input
+          value={cityState.state}
+          type="text"
+          name="state"
+          disabled
+          id="state"
+        />
+      </form>
+      <pre>
+        <code>{JSON.stringify(zipcode)}</code>
+      </pre>
     </div>
   );
 }
