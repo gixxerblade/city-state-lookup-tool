534ee80b7720282eefccb63498b4be67012ea118
gixxerblade <sdclarkie@gmail.com>
2020-09-16 17:40:56 -0400

    added xml parser, completed app

--- functions/getCityState/getCityState.js
+++ functions/getCityState/getCityState.js
@@ -1,47 +1,31 @@
-/* eslint-disable */
-// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
-const xml2js = require("xml2js");
 const fetch = require("node-fetch");
-const userId = process.env.REACT_APP_USERID;
-
-const xmlToJSON = (str, options) => {
-  return new Promise((resolve, reject) => {
-    xml2js.parseString(str, options, (err, jsonObj) => {
-      if (err) {
-        return reject(err);
-      }
-      resolve(jsonObj);
-    });
-  });
-};
-
-const lookup = (id, q) => {
-  return `<CityStateLookupRequest USERID=${id}><ZipCode ID='0'><Zip5>${q}</Zip5></ZipCode></CityStateLookupRequest>`;
-};
+const USERID = process.env.REACT_APP_USERID;
+const BASE_URI =
+  "http://production.shippingapis.com/ShippingAPITest.dll?API=CityStateLookup&XML=";
+const config = { headers: { "Content-Type": "text/xml" }, method: "get" };
 
 exports.handler = async function (event, context) {
-  const zipcode = event.queryStringParamenter.zipcode;
-  console.log(zipcode);
-  const url = `https://secure.shippingapis.com/ShippingAPI.dll?API= CityStateLookup&XML=`;
-  const config = { headers: { "Content-Type": "text/xml" }, method: "get" };
-
+  const zipcode = event.queryStringParameters.zipcode;
+  console.log("EVENT===", event.queryStringParameters);
+  const xml = `<CityStateLookupRequest USERID="${USERID}"><ZipCode ID="0"><Zip5>${zipcode}</Zip5></ZipCode></CityStateLookupRequest>`;
   try {
-    const response = await fetch(`${url}${lookup(userId, zipcode)}`, config);
+    console.log("ZIP: ", zipcode);
+    const response = await fetch(`${BASE_URI}${xml}`, config);
     if (!response.ok) {
-      // NOT res.status >= 200 && res.status < 300
-      return { statusCode: response.status, body: response.text() };
+      return { statusCode: response.status, body: response };
     }
     const data = await response.text();
-    console.log(data);
+
     return {
       statusCode: 200,
       body: data,
     };
   } catch (err) {
-    console.log(err); // output to netlify function log
+    console.log("Error: ", err);
     return {
       statusCode: 500,
-      body: JSON.stringify({ msg: err.message }), // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: err.message }),
     };
   }
 };
+
--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
-  const initialCityState = { city: "", state: "" };
-  const [zipcode, setZipcode] = useState("");
-  const [cityState, setCityState] = useState(initialCityState);
+const xml2json = (srcDOM) => {
+  let children = [...srcDOM.children];
+  // base case for recursion.
+  if (!children.length) {
+    return srcDOM.innerHTML;
+  }
+  // initializing object to be returned.
+  let jsonResult = {};
+  for (let child of children) {
+    // checking is child has siblings of same name.
+    let childIsArray =
+      children.filter((eachChild) => eachChild.nodeName === child.nodeName)
+        .length > 1;
+    // if child is array, save the values as array, else as strings.
+    if (childIsArray) {
+      if (jsonResult[child.nodeName] === undefined) {
+        jsonResult[child.nodeName] = [xml2json(child)];
+      } else {
+        jsonResult[child.nodeName].push(xml2json(child));
+      }
+    } else {
+      jsonResult[child.nodeName] = xml2json(child);
+    }
+  }
+  return jsonResult;
+};
 
+function App() {
+  const parser = new DOMParser();
+
+  const initialCityState = { city: "", state: "" };
+  // eslint-disable-next-line
+  const [cityState, setCityState] = useState(initialCityState);
+  const [zipcode, setZipcode] = useState("");
   const zipLen = zipcode.length === 5 && zipcode;
+
   useEffect(() => {
     const fetchCityState = async () => {
       try {
-        const response = await fetch(
-          `../functions/getCityState/${zipcode}`,
-          {}
-        );
-        const data = await response.text();
-        console.log(data);
+        if (zipLen) {
+          const response = await fetch(
+            `/.netlify/functions/getCityState?&zipcode=${zipcode}`,
+            {
+              headers: { accept: "application/json" },
+            }
+          );
+          const data = await response.text();
+          const srcDOM = parser.parseFromString(data, "application/xml");
+          console.log(xml2json(srcDOM));
+          const res = xml2json(srcDOM);
+          setCityState({
+            ...cityState,
+            city: res.CityStateLookupResponse.ZipCode.City,
+            state: res.CityStateLookupResponse.ZipCode.State,
+          });
+        }
       } catch (e) {
         console.log(e);
       }
     };
+
     fetchCityState();
     // eslint-disable-next-line
-  }, [zipLen]);
+  }, [zipcode]);
 
   return (
     <div className="App">
@@ -30,7 +72,7 @@ function App() {
       <form action="" className="form-data">
         <label htmlFor="zip">Zip Code Here</label>
         <input
-          value={zipcode}
+          value={zipcode || ""}
           placeholder="XXXXX"
           type="text"
           name="zip"
@@ -65,3 +107,4 @@ function App() {
 }
 
 export default App;
+//setCityState({...cityState, city: cityState.city, state: cityState.state})
