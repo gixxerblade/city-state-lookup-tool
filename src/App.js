81831b2f42f4117f7a9f5ac00f969e836321857d
gixxerblade <sdclarkie@gmail.com>
2020-09-17 10:33:45 -0400

    added error checking for invalid zip. added loading component to input boxes. minor styling. deleted src function folder

--- src/App.js
+++ src/App.js
@@ -35,12 +35,13 @@ function App() {
   // eslint-disable-next-line
   const [cityState, setCityState] = useState(initialCityState);
   const [zipcode, setZipcode] = useState("");
-  const zipLen = zipcode.length === 5 && zipcode;
+  const [loading, setLoading] = useState(false);
+  const isZipValid = zipcode.length === 5 && zipcode;
 
   useEffect(() => {
     const fetchCityState = async () => {
       try {
-        if (zipLen) {
+        if (isZipValid) {
           const response = await fetch(
             `/.netlify/functions/getCityState?&zipcode=${zipcode}`,
             {
@@ -51,11 +52,21 @@ function App() {
           const srcDOM = parser.parseFromString(data, "application/xml");
           console.log(xml2json(srcDOM));
           const res = xml2json(srcDOM);
-          setCityState({
-            ...cityState,
-            city: res.CityStateLookupResponse.ZipCode.City,
-            state: res.CityStateLookupResponse.ZipCode.State,
-          });
+          if (res?.CityStateLookupResponse?.ZipCode?.City) {
+            setLoading(false);
+            setCityState({
+              ...cityState,
+              city: res.CityStateLookupResponse.ZipCode.City,
+              state: res.CityStateLookupResponse.ZipCode.State,
+            });
+          } else if (res?.CityStateLookupResponse?.ZipCode?.Error) {
+            setLoading(false);
+            setCityState({
+              ...cityState,
+              city: `Invalid Zip Code for ${zipcode}`,
+              state: "Try Again",
+            });
+          }
         }
       } catch (e) {
         console.log(e);
@@ -63,15 +74,15 @@ function App() {
     };
 
     fetchCityState();
-    // eslint-disable-next-line
   }, [zipcode]);
 
   return (
     <div className="App">
       <h1>City/State Lookup Tool</h1>
       <form action="" className="form-data">
-        <label htmlFor="zip">Zip Code Here</label>
+        <label htmlFor="zip">Type Zip Code Here</label>
         <input
+          className="zip"
           value={zipcode || ""}
           placeholder="XXXXX"
           type="text"
@@ -79,32 +90,51 @@ function App() {
           id="zip"
           onChange={(event) => {
             const { value } = event.target;
+            setLoading(true);
+            setCityState(initialCityState);
             setZipcode(value.replace(/[^\d{5}]$/, "").substr(0, 5));
           }}
         />
-        <label htmlFor="city">City Here</label>
-        <input
-          value={cityState.city}
-          type="text"
-          name="city"
-          disabled
-          id="city"
-        />
-        <label htmlFor="state">State Here</label>
-        <input
-          value={cityState.state}
-          type="text"
-          name="state"
-          disabled
-          id="state"
-        />
+        <label htmlFor="city">City</label>
+        <div className="input-container">
+          <input
+            className={`city`}
+            value={cityState.city}
+            type="text"
+            name="city"
+            disabled
+            id="city"
+          />
+          <div className="icon-container">
+            <i className={`${loading && isZipValid === 5 ? "loader" : ""}`}></i>
+          </div>
+        </div>
+        <label htmlFor="state">State</label>
+        <div className="input-container">
+          <input
+            className={`state`}
+            value={cityState.state}
+            type="text"
+            name="state"
+            disabled
+            id="state"
+          />
+          <div className="icon-container">
+            <i className={`${loading && isZipValid ? "loader" : ""}`}></i>
+          </div>
+        </div>
       </form>
       <pre>
-        <code>{JSON.stringify(zipcode)}</code>
+        <code>
+          {JSON.stringify({
+            zipcode: zipcode,
+            city: cityState.city,
+            state: cityState.state,
+          })}
+        </code>
       </pre>
     </div>
   );
 }
 
 export default App;
-//setCityState({...cityState, city: cityState.city, state: cityState.state})
